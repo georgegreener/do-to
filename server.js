0f2c06a966b8a15310b7f4b669b519cc4278a9d0
George Greene <georgegreene@outlook.com>
2018-10-04 20:37:21 -0400

    initial server build

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+var port = process.env.port || 3000;
+
+express().use(bodyParser.urlencoded({ extended: true }));
+
+express().use(bodyParser.json());
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/do-to";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+
+express().listen(port, function() {
+    console.log("This is Dr. Frasier Crane... I'm listening...\nPort: " + port);
+  });
